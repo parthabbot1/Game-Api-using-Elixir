0d139fba9d08b88b948b0e5a252b7a103416312f
parth <parth.abbot@gmail.com>
2023-12-19 17:32:43 +0530

    change

--- lib/challenge/user_worker.ex
+++ lib/challenge/user_worker.ex
@@ -142,7 +142,7 @@ defmodule Challenge.UserWorker do
     {:ok, %{state | user: new_user, wins: new_wins}}
   end
 
-  # Please note we are checkhing the idempotency at user level as well keeping
+  # Please note we are checking the idempotency at user level as well keeping
   #  the scalability of app in mind. in the current scenario it is not required
   # but in future it may be required
 
